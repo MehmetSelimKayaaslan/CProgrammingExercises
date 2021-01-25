d078efa5e4c95e982bcb00bbf54eb3907f42ba29
seyda <seyda@turna.tech>
2021-01-25 11:27:57 +0300

    programlama sorusu 1 çözüldü

---
+++ Chapter2/ProgrammingExercise1.c
@@ -0,0 +1,11 @@
+//
+// Created by seydakoclar on 25.01.2021.
+// Soru1:
+// sdgfsjdhfgjshdfgjshdgfsjdgfjsdghf
+//sdfhsjdfgsjdfgsjdhfgsjdfgsdjfg
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+}
