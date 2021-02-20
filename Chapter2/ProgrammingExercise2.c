181fe474ae6d75ad410ee79262d2cda4b050d4d8
Mehmet Selim <meselimka@gmail.com>
2021-02-20 14:28:52 +0300

    Added new program

---
+++ Chapter2/ProgrammingExercise2.c
@@ -0,0 +1,11 @@
+//Soru 2:
+//Girilen büyük bir harfi küçük bir harfe çeviren programı yazınız.
+#include <stdio.h>
+int main(){
+    char x;
+    scanf("%c", &x);
+    x += 32;
+    printf("%c\n", x);
+
+}
+
